363c04a509912ac79b2c9effdb356f4208f26b46
GabrielEmerson <gabrielemerson.dev@gmail.com>
2024-01-22 17:30:27 -0300

    Feat: Criação de Controller e Service de City

---
+++ src/city/city.controller.ts
@@ -0,0 +1,8 @@
+import { Controller, Get, Post, Put, Patch, Body } from '@nestjs/common';
+import { CityEntity } from './entities/city.entity';
+import { CityService } from './city.service';
+
+@Controller('city')
+export class CityController {
+  constructor(private readonly cityService: CityService) {}
+}
