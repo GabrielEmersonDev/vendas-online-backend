248f3f461f9165eb5a80e005f6e91082cba858f1
GabrielEmerson <gabrielemerson.dev@gmail.com>
2024-01-23 16:38:30 -0300

    feat: export e import de cache para module de city

--- src/cache/cache.module.ts
+++ src/cache/cache.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CacheService } from './cache.service';
+import { CacheModule as CacheModuleNest } from '@nestjs/cache-manager';
 
 @Module({
-  providers: [CacheService]
+  imports: [
+    CacheModuleNest.register({
+      ttl: 900000000,
+    }),
+  ],
+  providers: [CacheService],
+  exports: [CacheService],
 })
 export class CacheModule {}
