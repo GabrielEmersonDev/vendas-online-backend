00e00776e8cc6cced8092127825e6b5cceb011a5
GabrielEmerson <gabrielemerson.dev@gmail.com>
2024-01-23 18:50:31 -0300

    feat: utilização de validation pipe

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,19 +1,32 @@
-import { Controller, Get, Post, Put, Patch, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Patch,
+  Body,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
 import { UserService } from './user.service';
 import { UserEntity } from './entities/user.entity';
+import { ReturnUserDto } from './dtos/returnUser.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @UsePipes(ValidationPipe)
   @Post()
   async createUser(@Body() createUser: CreateUserDto): Promise<UserEntity> {
     return this.userService.createUser(createUser);
   }
 
   @Get()
-  async getAllUser(): Promise<UserEntity[]> {
-    return this.userService.getAlUser();
+  async getAllUser(): Promise<ReturnUserDto[]> {
+    return (await this.userService.getAlUser()).map(
+      (userEntity) => new ReturnUserDto(userEntity),
+    );
   }
 }
