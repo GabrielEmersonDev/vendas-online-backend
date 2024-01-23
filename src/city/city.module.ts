5364cf56c7b9aa8664561e6e53fa0b02e9599d84
GabrielEmerson <gabrielemerson.dev@gmail.com>
2024-01-23 17:04:56 -0300

    feat: remoção de import desnecessario em city module

--- src/city/city.module.ts
+++ src/city/city.module.ts
@@ -3,18 +3,10 @@ import { CityController } from './city.controller';
 import { CityService } from './city.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CityEntity } from './entities/city.entity';
-import { CacheModule as CacheModuleNest } from '@nestjs/cache-manager';
 import { CacheModule } from 'src/cache/cache.module';
 
 @Module({
-  imports: [
-    CacheModuleNest.register({
-      ttl: 900000000,
-    }),
-
-    CacheModule,
-    TypeOrmModule.forFeature([CityEntity]),
-  ],
+  imports: [CacheModule, TypeOrmModule.forFeature([CityEntity])],
   controllers: [CityController],
   providers: [CityService],
 })
