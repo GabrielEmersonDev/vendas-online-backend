61e1e023a8f550ac3b5bd3f0137e5a9d742079fc
GabrielEmerson <gabrielemerson.dev@gmail.com>
2024-01-26 19:13:38 -0300

    feat: insert DTO's para city e state

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -32,9 +32,7 @@ export class UserController {
   }
 
   @Get('/:userId')
-  async getUserById(@Param('userId') userId: number): Promise<ReturnUserDto> {
-    return new ReturnUserDto(
-      await this.userService.getUserByIdUsingRelations(userId),
-    );
+  async getUserById(@Param('userId') userId: number): Promise<UserEntity> {
+    return this.userService.getUserByIdUsingRelations(userId);
   }
 }
