2e369615388bcff7cab3eb2e76df0101171d14cc
GabrielEmerson <gabrielemerson.dev@gmail.com>
2024-03-17 01:17:19 -0300

    feat: mock e test for product

---
+++ src/product/__mocks__/product.mock.ts
@@ -0,0 +1,12 @@
+import { categoryMock } from '../../category/__mocks__/category.mock';
+import { ProductEntity } from '../entities/product.entity';
+
+export const stateEntityMock: ProductEntity = {
+  name: 'Product 1',
+  price: 34.3,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  imagem: 'http://image.com',
+  id: 124,
+  categoryId: categoryMock.id,
+};
