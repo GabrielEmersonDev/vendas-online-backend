d8e80e8e34cce2b7c9d517e330fb0b1ffb65deba
GabrielEmerson <gabrielemerson.dev@gmail.com>
2024-03-26 01:11:59 -0300

    feat: novos verificar productId e return cart dto

--- src/cart/dtos/return-cart.dto.ts
+++ src/cart/dtos/return-cart.dto.ts
@@ -1,13 +1,18 @@
 import { CartProductEntity } from 'src/cart-product/entities/cart-product.entity';
 import { CartEntity } from '../entities/cart.entity';
+import { ReturnCartProductDto } from '../../cart-product/dtos/return-cart-product.dto';
 
 export class ReturnCartDto {
   id: number;
 
-  cartProduct?: CartProductEntity[];
+  cartProduct?: ReturnCartProductDto[];
 
   constructor(cart: CartEntity) {
     this.id = cart.id;
-    this.cartProduct = cart.cartProduct;
+    this.cartProduct = cart.cartProduct
+      ? cart.cartProduct.map(
+          (cartProduct) => new ReturnCartProductDto(cartProduct),
+        )
+      : undefined;
   }
 }
