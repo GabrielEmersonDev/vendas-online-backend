572ae8a97c5b8b79d711424a1811a9b1a4315774
GabrielEmerson <gabrielemerson.dev@gmail.com>
2024-05-28 03:23:33 -0300

    feat: nova relação para productos

--- src/product/__tests__/product.service.spec.ts
+++ src/product/__tests__/product.service.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { In, Repository } from 'typeorm';
 import { ProductService } from '../product.service';
 import { ProductEntity } from '../entities/product.entity';
 import { productEntityMock } from '../__mocks__/product.mock';
@@ -53,6 +53,33 @@ describe('ProductService', () => {
     const products = await service.findAll();
 
     expect(products).toEqual([productEntityMock]);
+  });
+
+  it('should return relations in find all products', async () => {
+    const spy = jest.spyOn(productRepository, 'find');
+    const products = await service.findAll([], true);
+
+    expect(products).toEqual([productEntityMock]);
+    expect(spy.mock.calls[0][0]).toEqual({
+      relations: {
+        category: true,
+      },
+    });
+  });
+
+  it('should return relatiosn and array in find all products', async () => {
+    const spy = jest.spyOn(productRepository, 'find');
+    const products = await service.findAll([1], true);
+
+    expect(products).toEqual([productEntityMock]);
+    expect(spy.mock.calls[0][0]).toEqual({
+      where: {
+        id: In([1]),
+      },
+      relations: {
+        category: true,
+      },
+    });
   });
 
   it('should return error if products empty', async () => {
--- src/product/dto/return-product.dto.ts
+++ src/product/dto/return-product.dto.ts
@@ -1,3 +1,4 @@
+import { ReturnCategoryDto } from '../../category/dtos/return-category.dto';
 import { ProductEntity } from '../entities/product.entity';
 
 export class ReturnProduct {
@@ -5,11 +6,15 @@ export class ReturnProduct {
   name: string;
   price: number;
   image: string;
+  category?: ReturnCategoryDto;
 
   constructor(productEntity: ProductEntity) {
     this.id = productEntity.id;
     this.name = productEntity.name;
     this.price = productEntity.price;
     this.image = productEntity.image;
+    this.category = productEntity.category
+      ? new ReturnCategoryDto(productEntity.category)
+      : undefined;
   }
 }
