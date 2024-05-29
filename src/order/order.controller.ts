7e1fcb9a0f354202d98d7d34458818ce6e4205c0
GabrielEmerson <gabrielemerson.dev@gmail.com>
2024-05-28 21:59:11 -0300

    feat: relations to order

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -10,7 +10,11 @@ import { CreateOrderDto } from './dtos/create-order.dto';
 import { OrderService } from './order.service';
 import { UserId } from '../decorators/user-id.decorator';
 import { OrderEntity } from './entities/order.entity';
+import { UserType } from '../user/enum/user-type.enum';
+import { Roles } from '../decorators/roles.decorator';
+import { ReturnOrderDTO } from './dtos/return-order.dto';
 
+@Roles(UserType.Admin, UserType.User)
 @Controller('order')
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
@@ -25,5 +29,13 @@ export class OrderController {
   @Get()
   async findOrdersByUserId(@UserId() userId: number): Promise<OrderEntity[]> {
     return this.orderService.findOrdersByUserId(userId);
+  }
+
+  @Roles(UserType.Admin)
+  @Get('/all')
+  async findAllOrders(): Promise<ReturnOrderDTO[]> {
+    return (await this.orderService.findAllOrders()).map(
+      (order) => new ReturnOrderDTO(order),
+    );
   }
 }
